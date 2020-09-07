7300c8d408cfe05894c55433d023f3c6cb2c0e4c
sanSkiller <1290148953@qq.com>
2020-09-07 19:59:14 +0800

    updated:特性测试区块组件

---
+++ examples/Blockarea.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="block-area">
+    <h2>{{ title }}</h2>
+    <hr />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blockarea",
+  props: {
+    title: {
+      type: String,
+      default: "特性测试区",
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.block-area {
+  margin-bottom: 50px;
+}
+</style>
