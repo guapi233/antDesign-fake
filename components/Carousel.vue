b6a1baef9bc841401d49515c34cdb8bda7545eaf
sanSkiller <1290148953@qq.com>
2020-09-13 18:57:51 +0800

    updated:Carousel组件不同位置的按钮

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="c-carousel" ref="carousel">
-    <div class="c-carousel__wrapper" :style="{ ...wrapperSize, ...handleSlide }">
+    <div class="c-carousel__wrapper" :style="{ ...wrapperSize, ...handleSlide }" ref="wrapper">
       <slot></slot>
     </div>
-    <ul class="c-carousel__dotlist">
+    <ul class="c-carousel__dotlist" :class="`c-carousel--dotPosition__${ dotPosition }`">
       <li
         class="dot-item"
         v-for="(item, index) in dotList"
@@ -20,6 +20,12 @@
 <script>
 export default {
   name: "CCarousel",
+  props: {
+    dotPosition: {
+      type: String,
+      default: "bottom",
+    },
+  },
   data() {
     return {
       itemWidth: 0, // 单个展示项的宽
@@ -31,11 +37,26 @@ export default {
     };
   },
   computed: {
+    // 走马灯是否向右滑动
+    isRightSlide() {
+      return ["top", "bottom"].includes(this.dotPosition);
+    },
+    // 走马灯是否向上滑动
+    isTopSlide() {
+      return ["left", "right"].includes(this.dotPosition);
+    },
     // 处理展示变化
     handleSlide() {
-      return {
-        transform: `translateX(-${(this.curActive + 1) * this.itemWidth}px)`,
-      };
+      // 判断变化方式以及方向
+      if (this.isRightSlide) {
+        return {
+          transform: `translateX(-${(this.curActive + 1) * this.itemWidth}px)`,
+        };
+      } else if (this.isTopSlide) {
+        return {
+          transform: `translateY(-${(this.curActive + 1) * this.itemHeight}px)`,
+        };
+      }
     },
     // 控制按钮长度
     dotList() {
@@ -45,27 +66,48 @@ export default {
     },
   },
   methods: {
+    // 计算布局
+    calculateLayout() {
+      if (this.isRightSlide) {
+        this.$refs["wrapper"].style.display = "flex";
+      } else {
+        this.$refs["wrapper"].style.display = "block";
+      }
+    },
     // 初始化包裹宽高
     initWrapperSize() {
-      // 为插槽内的元素设置固定宽高与排列方式
-      this.$slots.default.forEach((item) => {
-        item.elm.style.width = this.$refs["carousel"].clientWidth + "px";
-        item.elm.style.display = "inline-block";
-        // item.elm.style.height = this.$refs["carousel"].clientHeight + "px";
-      });
+      // 为插槽内的元素设置固定宽与排列方式
+      this.calculateLayout();
 
-      // 计算总包裹层总宽高
+      // 计算单元素高度与总包裹层总宽高
       let itemCount = this.$slots.default.length;
+
       this.itemWidth = this.$refs["carousel"].clientWidth;
-      this.itemHeight = this.$refs["carousel"].clientHeight;
+      this.itemHeight = this.$refs[
+        "carousel"
+      ].children[0].children[0].clientHeight;
+
       this.itemCountWidth = this.$refs["carousel"].clientWidth * itemCount;
       this.itemCountHeight = this.$refs["carousel"].clientHeight * itemCount;
 
-      this.wrapperSize = { width: this.itemCountWidth + "px" };
-      console.log(this.wrapperSize);
+      // 为单个元素添加宽高
+      this.$slots.default.forEach((item) => {
+        item.elm.style.width = this.itemWidth + "px";
+        item.elm.style.height = this.itemHeight + "px";
+      });
+
+      // 为外层包裹添加宽高
+      if (this.isRightSlide) {
+        this.wrapperSize = { width: this.itemCountWidth + "px" };
+      } else if (this.isTopSlide) {
+        this.wrapperSize = { Height: this.itemCountHeight + "px" };
+      }
+
+      // 给最外层元素设置固定高度，方便配合overflow裁切可视区
+      this.$refs["carousel"].style.height = this.itemHeight + "px";
     },
-    // 初始化内容布局
-    initLayout() {
+    // 初始化元素数量
+    initItemCount() {
       this.$slots.default.unshift({
         ...this.$slots.default[this.$slots.default.length - 1],
       });
@@ -73,9 +115,25 @@ export default {
       this.$slots.default.push({ ...this.$slots.default[1] });
     },
   },
+  watch: {
+    dotPosition(newVal, oldVal) {
+      // 重新计算元素数量
+      this.initItemCount();
+
+      // 重新计算布局
+      this.calculateLayout();
+
+      // 重新金酸包裹层高度
+      if (this.isRightSlide) {
+        this.wrapperSize = { width: this.itemCountWidth + "px" };
+      } else if (this.isTopSlide) {
+        this.wrapperSize = { height: this.itemCountHeight + "px" };
+      }
+    },
+  },
   created() {
     // 初始化内容布局方式
-    this.initLayout();
+    this.initItemCount();
     window.addEventListener("resize", this.initWrapperSize);
   },
   mounted() {
@@ -96,20 +154,13 @@ export default {
 
   &__dotlist {
     position: absolute;
-    bottom: 12px;
-    left: 50%;
-    transform: translateX(-50%);
     list-style: none;
     padding: 0;
-    display: flex;
     margin: 0;
 
     .dot-item {
-      margin-right: 8px;
-
       .dot-btn {
-        width: 16px;
-        height: 3px;
+        display: block;
         font-size: 0;
         color: transparent;
         background: #fff;
@@ -121,12 +172,69 @@ export default {
         outline: none;
       }
     }
+  }
+}
 
-    .is-active {
-      .dot-btn {
-        width: 24px;
-        opacity: 1;
-      }
+// 不同的按钮位置
+.c-carousel--dotPosition__top {
+  display: flex;
+  top: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.c-carousel--dotPosition__bottom {
+  display: flex;
+  bottom: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.c-carousel--dotPosition__top,
+.c-carousel--dotPosition__bottom {
+  .dot-item {
+    margin-right: 8px;
+    .dot-btn {
+      width: 16px;
+      height: 3px;
+    }
+  }
+
+  .is-active {
+    .dot-btn {
+      width: 24px;
+      opacity: 1;
+    }
+  }
+}
+
+.c-carousel--dotPosition__left {
+  left: 12px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.c-carousel--dotPosition__right {
+  right: 12px;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.c-carousel--dotPosition__left,
+.c-carousel--dotPosition__right {
+  .dot-item {
+    margin-bottom: 8px;
+    .dot-btn {
+      padding: 0;
+      width: 3px;
+      height: 16px;
+    }
+  }
+
+  .is-active {
+    .dot-btn {
+      height: 24px;
+      opacity: 1;
     }
   }
 }
--- examples/test/Carousel.vue
+++ examples/test/Carousel.vue
@@ -16,12 +16,40 @@
         </div>
       </c-carousel>
     </blockarea>
+
+    <blockarea title="不同位置的按钮">
+      <c-radio-group v-model="dotPosition">
+        <c-radio label="top"></c-radio>
+        <c-radio label="right"></c-radio>
+        <c-radio label="bottom"></c-radio>
+        <c-radio label="left"></c-radio>
+      </c-radio-group>
+      <c-carousel :dotPosition="dotPosition">
+        <div class="item">
+          <h3>1</h3>
+        </div>
+        <div class="item">
+          <h3>2</h3>
+        </div>
+        <div class="item">
+          <h3>3</h3>
+        </div>
+        <div class="item">
+          <h3>4</h3>
+        </div>
+      </c-carousel>
+    </blockarea>
   </div>
 </template>
 
 <script>
 export default {
   name: "Carousel",
+  data() {
+    return {
+      dotPosition: "bottom",
+    };
+  },
 };
 </script>
 
@@ -34,5 +62,9 @@ export default {
   overflow: hidden;
   color: white;
   font-weight: 600;
+
+  h3 {
+    margin: 0;
+  }
 }
 </style>
